b92f6dc7fb5f13e1dc05555f06ce506c0478abfb
Gabriel <developer.gabriel@gmail.com>
2016-02-16 17:53:19 -0200

    Exercicios até capitulo 6

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,36 +1,15 @@
-angular.module('projeto')
-    .controller('FotoController', function($scope, $routeParams, $location, FotoService) {
-
-        $scope.foto = {};
-        $scope.mensagem = '';
-    
-        if($routeParams.fotoId) {
-            FotoService.get({fotoId: $routeParams.fotoId}, function(retorno) {
-                 $scope.foto = retorno;
-            });
+angular.module('alurapic')
+.controller('FotoController', function($scope, $http) {
+    $scope.submeter = function() {
+        if($scope.formulario.$valid) {
+            $http.post('v1/fotos', $scope.foto)
+            .success(function() {
+                $scope.mensagem = 'Foto cadastrada com sucesso';
+            })
+            .error(function(erro) {
+                console.log(erro);
+                $scope.mensagem = 'Não foi possivel cadastrar foto';  
+            });  
         }
-
-        $scope.submeter = function() {
-           if($scope.formulario.$valid) {
-                if(!$routeParams.fotoId) {
-                    
-                    FotoService.save($scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Salvo com sucesso';
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível salvar';
-                    });
-
-                } else {
-                    FotoService.update({fotoId : $scope.foto._id}, $scope.foto, function(_id) {
-                        $scope.foto = {};
-                        $scope.mensagem = 'Alterado com sucesso';                        
-                    }, function(status) {
-                        console.log(status);
-                        $scope.mensagem = 'Não foi possível alterar';
-                    });                    
-                }
-           } 
-        };
-    });
+    };
+})
