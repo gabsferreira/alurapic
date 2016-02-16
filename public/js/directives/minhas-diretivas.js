130077093952acec20534069f2569e49e9570fcd
Gabriel <developer.gabriel@gmail.com>
2016-02-16 19:40:27 -0200

    Até exercicio 8

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -28,3 +28,18 @@ angular.module('minhasDiretivas')
 
   return ddo;
 });
+
+angular.module('minhasDiretivas')
+.directive('meuBotaoPerigo', function() {
+  var ddo = {};
+
+  ddo.restrict = "E";
+  ddo.scope = {
+    nome: '@',
+    acao: '&'
+  };
+
+  ddo.template = '<button class="btn btn-danger btn-block" ng-click="acao()">{{nome}}</button>';
+
+  return ddo;
+});
