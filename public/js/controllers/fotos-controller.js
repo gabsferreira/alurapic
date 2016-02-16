1943a5fcee9852851d5ff3161a3169eca29b1cd2
Gabriel <developer.gabriel@gmail.com>
2016-02-16 18:38:54 -0200

    Até capitulo 7

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -19,4 +19,18 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
   .error(function(erro) {
     console.log(erro);
   });
+  
+  $scope.remover = function(foto) {
+      $http.delete('/v1/fotos/' + foto._id)
+      .success(function() {
+          var indiceDaFoto = $scope.fotos.indexOf(foto);
+          $scope.fotos.splice(indiceDaFoto, 1);
+          $scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
+      })
+      .error(function(erro) {
+          console.log(erro);
+          $scope.mensagem = 'Não foi possível carregar a foto ' + foto.titulo;
+      });
+  }
+  
 });
